f98ae50416f9081b94981c6d7671c97c8e6a1aa0
Ермагамбетов Данияр Альнурович <ErmagambetovDa@lcoast.itstep>
2024-11-25 21:01:01 +0500

    commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# task 1
+
+#
+# number1 = int(input("write first number: "))
+# number2 = int(input("write second number: "))
+# number3 = int(input("write third number: "))
+#
+# print(number1 + number2 + number3, "sum of three numbers")
+# print(number1 * number2 * number3, "no sum of three numbers")
+
+
+# task 2
+#
+# salary = int(input("Введите свою зарплату: "))
+# credit = int(input("Ваш кредит: "))
+# ipoteka = int(input("Ваша ипотека: "))
+#
+# print(salary - credit + ipoteka)
+
+
+#task 3
+
+d1 = int(input())
+d2 = int(input())
+result = (d1 * d2) / 2
+print(result, " - площадь ромба")
